f72cea49fa535b203d6b0cdfcd861124ee4c35bc
MercX <freddiecharlesjnr@gmail.com>
2022-05-23 02:46:59 +0100

    mains got the int

--- main.h
+++ main.h
@@ -5,7 +5,7 @@
 int _putchar(char c);
 int _printf(const char * const format, ...);
 int _print_char(va_list args);
-int _print_numbers(va_list args);
+int _print_int(va_list args);
 int _print_str(va_list args);
 int (*_select_func(char c))(va_list)
 
